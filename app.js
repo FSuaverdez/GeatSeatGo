a81c6e20c7ef7a12f2cbc91ff40bcf4e24dd6b3f
FSuaverdez <sfrannz@gmail.com>
2020-10-26 16:26:22 +0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+
+// middleware
+app.use(express.static('public'))
+
+// view engine
+app.set('view engine', 'ejs')
+
+// database connection
+const dbURI =
+  'mongodb+srv://admin:admin123@cluster0.sexkk.mongodb.net/movie-ticket'
+mongoose
+  .connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then((result) => {
+    app.listen(3000)
+    console.log('MongoDB connected')
+    console.log('Listening to port 3000')
+  })
+  .catch((err) => console.log(err))
+
+// routes
+app.get('/', (req, res) => res.render('home'))
+app.get('/smoothies', (req, res) => res.render('smoothies'))
