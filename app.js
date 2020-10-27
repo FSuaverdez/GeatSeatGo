c75d5a0983af1240c7e797d450ee7295612ccf14
FSuaverdez <sfrannz@gmail.com>
2020-10-27 18:10:10 +0800

    Added Cookie Parser

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const mongoose = require('mongoose')
 const authRouter = require('./routes/authRoutes')
 const dotenv = require('dotenv')
+const cookieParser = require('cookie-parser')
+
 const app = express()
 
 const PORT = process.env.PORT || 3000
@@ -9,6 +11,7 @@ const PORT = process.env.PORT || 3000
 // middleware
 app.use(express.static('public'))
 app.use(express.json())
+app.use(cookieParser())
 
 // view engine
 app.set('view engine', 'ejs')
@@ -34,3 +37,18 @@ mongoose
 app.get('/', (req, res) => res.render('home'))
 app.get('/smoothies', (req, res) => res.render('smoothies'))
 app.use(authRouter)
+
+app.get('/set-cookies', (req, res) => {
+  res.cookie('newUser', false)
+  res.cookie('isEmployee', true, {
+    maxAge: 1000 * 60 * 60 * 24,
+    httpOnly: true,
+  })
+  res.send('you got the cookies!')
+})
+
+app.get('/read-cookies', (req, res) => {
+  const cookies = req.cookies
+  console.log(cookies)
+  res.json(cookies)
+})
