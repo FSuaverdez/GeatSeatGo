b2e666fcaa059aeb2fec32024b4a922f1a33d2d8
FSuaverdez <sfrannz@gmail.com>
2020-10-27 20:00:48 +0800

    Added Azure

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,3 +1,9 @@
 module.exports.admin_get = (req, res) => {
   res.render('./pages/admin/adminPage')
 }
+module.exports.admin_login_get = (req, res) => {
+  res.render('./pages/admin/adminLogin')
+}
+module.exports.admin_login_post = (req, res) => {
+  res.render('./pages/admin/adminLogin')
+}
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -4,7 +4,7 @@ const adminController = require('../controllers/adminController')
 const router = Router()
 
 router.get('/admin', adminController.admin_get)
-router.get('/admin/login', authController.login_get)
-router.post('/admin/login', authController.login_post)
+router.get('/admin/login', adminController.admin_login_get.login_get)
+router.post('/admin/login', adminController.admin_login_get.login_post)
 
 module.exports = router
