7573e09d69724fc1002cc1abffc596b9884c8d3c
FSuaverdez <sfrannz@gmail.com>
2020-10-27 20:32:27 +0800

    Fixed Errors

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -4,6 +4,4 @@ module.exports.admin_get = (req, res) => {
 module.exports.admin_login_get = (req, res) => {
   res.render('./pages/admin/adminLogin')
 }
-module.exports.admin_login_post = (req, res) => {
-  res.render('./pages/admin/adminLogin')
-}
+module.exports.admin_login_post = (req, res) => {}
