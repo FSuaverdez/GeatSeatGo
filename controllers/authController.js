f613f18435efa328090af41dd541d4449cd3111b
FSuaverdez <sfrannz@gmail.com>
2020-10-27 16:13:17 +0800

    added more options for creting user

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,25 @@
 const User = require('../models/User')
+const mongoose = require('mongoose')
+
+const handleErrors = (err) => {
+  const errors = { email: '', password: '' }
+
+  // Validate if email is unique
+  if (err.name === 'MongoError' && err.code === 11000) {
+    errors['email'] = 'Email already registered'
+    return errors
+  }
+
+  // Validate email and password length
+  if (err.message.includes('user validation failed')) {
+    Object.values(err.errors).forEach(({ properties }) => {
+      console.log(properties)
+      errors[properties.path] = properties.message
+    })
+  }
+
+  return errors
+}
 
 module.exports.signup_get = (req, res) => {
   res.render('signup')
@@ -12,15 +33,22 @@ module.exports.signup_post = async (req, res) => {
   const { email, password } = req.body
 
   try {
+    let id = new mongoose.Types.ObjectId().toHexString()
+    let createdBy = id
+    let modifiedBy = id
+
     const user = await User.create({
+      _id: id,
       email,
       password,
       role: 'ADMIN',
+      createdBy,
+      modifiedBy,
     })
     return res.status(201).json(user)
   } catch (error) {
-    console.log(error)
-    return res.status(400).send('user, not created')
+    const errors = handleErrors(error)
+    return res.status(400).json({ errors })
   }
 
   res.send('New Signup')
