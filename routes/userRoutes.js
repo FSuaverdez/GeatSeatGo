ed63b662a8f70bfa38542141afd44933742ec980
FSuaverdez <sfrannz@gmail.com>
2020-12-21 19:14:37 +0800

    Fix Crash

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,4 @@
 const { Router } = require('express')
-const request = require('request')
 const {
   schedule_get,
   schedulesId_get,
@@ -7,15 +6,6 @@ const {
 const { movies_get } = require('../controllers/movieController')
 const { movieSlug_get } = require('../controllers/movieController')
 const { requireAuth } = require('../middlewares/authMiddleware')
-const paypal = require('paypal-rest-sdk')
-
-paypal.configure({
-  mode: 'sandbox', //sandbox or live
-  client_id:
-    'AS0OYjYmD_LErfQVR-HptUpYDyRppEksv0HIXoB0ffSf1sufasjp64RCJdVkCmVDThFo9klYQQ1Ub0cL',
-  client_secret:
-    'EEd3Mm6m2-k1V825Cp_wIOFds6_34-zDCN57MwW0lEnTl4wAUag0PFBFBXYGVvgcDMu9LLfBUCw-MRzq',
-})
 
 const router = Router()
 
